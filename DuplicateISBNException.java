92957c359bf41745313fb7d5fb20a0ad428c8d90
Andrés Lou <lourios.andres@gmail.com>
2015-10-19 00:48:53 -0400

    Added first draft of DuplicateISBNException.java

---
+++ DuplicateISBNException.java
@@ -0,0 +1,17 @@
+public class DuplicateISBNException extends Exception{
+	
+	//Default constructor
+	public DuplicateISBNException(){
+		super("Duplicate ISBN Exception!");
+	}
+	//Parametrised constructor
+	public DuplicateISBNException(String message){
+		super(message);
+	}
+
+
+	//Error message
+	public String getMessage(){
+		return this.message;
+	}
+}
