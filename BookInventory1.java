61ae6e3bdc1a1c0c57b8cfb6f1a70bae81e8646b
Andrés Lou <lourios.andres@gmail.com>
2015-10-25 15:35:59 -0400

    holy shit it actually compiled

--- BookInventory1.java
+++ BookInventory1.java
@@ -1,5 +1,4 @@
-//import Book;
-//import DuplicateISBNException;
+import java.io.*;
 import java.util.Scanner;
 
 public class BookInventory1{
@@ -8,9 +7,8 @@ public class BookInventory1{
 
 		//number of books in input file
 		int nBooks;
-
-		Book[] BkArray;
 		Scanner kb = new Scanner(System.in);
+		String line = null;
 
 		//name of the corrected ISBN catalog file to be created
 		String correctedFileName = null;
@@ -22,8 +20,46 @@ public class BookInventory1{
 
 		//TO DO: check for existing files of that name. In case
 		//user gives the name of a preexisting file, reject input,
-		//display file size and repeat query
+		//display file susize and repeat query
 
+		try{
+			FileReader fileReader = new FileReader("Initial_Book_Info.txt");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			//count how many lines the file has to initialise the book array
+			int lines = 0;
+			while (bufferedReader.readLine() != null){lines++;}
+			bufferedReader.close();
+
+			//initialise array of books
+			Book[] BkArray = new Book[lines];
+
+			//bookIndex increases as BkArray is populated
+			int bookIndex = 0;
+
+			//read file line-by-line and create a book object for each line
+			while((line = bufferedReader.readLine()) != null) {
+				//each line in the file is split using whitespace as delimiter
+				//and each token used as a parameter for the book object
+				Book book = new Book(
+					Long.parseLong(line.split("\\s+")[0]),
+					line.split("\\s+")[1],
+					Integer.parseInt(line.split("\\s+")[2]),
+					line.split("\\s+")[3],
+					Double.parseDouble(line.split("\\s+")[4]),
+					Integer.parseInt(line.split("\\s+")[5])
+					);
+				BkArray[bookIndex] = book;
+				bookIndex++;
+			}
+			bufferedReader.close();
+		}
+
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + correctedFileName + "'");  
+        }
 		//TO DO: read file, determine number of books contained and
 		//define BkArray.
 
